5a334010be0ad41ee977375406f51f350fa97ca3
JP Robinson <john.paul.robinson@gmail.com>
2019-10-29 20:57:05 -0400

    NOT reusing the same *http.Client pointer to avoid after effects

--- gcpvault.go
+++ gcpvault.go
@@ -245,12 +245,15 @@ func newJWTBase(ctx context.Context, cfg Config) (string, error) {
 	}
 
 	hc := getHTTPClient(ctx, cfg)
-	// reuse base transport but sprinkle on the token source for IAM access
-	hc.Transport = &oauth2.Transport{
-		Source: tokenSource,
-		Base:   hc.Transport,
+	// reuse base transport and timeout but sprinkle on the token source for IAM access
+	hcIAM := &http.Client{
+		Timeout: hc.Timeout,
+		Transport: &oauth2.Transport{
+			Source: tokenSource,
+			Base:   hc.Transport,
+		},
 	}
-	iamClient, err := iam.New(hc)
+	iamClient, err := iam.New(hcIAM)
 	if err != nil {
 		return "", errors.Wrap(err, "unable to init IAM client")
 	}
